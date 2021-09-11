3171d6d6a4b6356efb547eb469bb0d6765acf935
MatsubaraTsu <85394676+MatsubaraTsu@users.noreply.github.com>
2021-09-11 17:37:24 +0900

    Magic関数11回呼び出し、mp消費、魔法が使えた場合と使えない場合でログを分けました。

--- Lesson4.cs/Lesson4.cs
+++ Lesson4.cs/Lesson4.cs
@@ -1,9 +1,9 @@
 // Hello World プログラムを C# でRPG用に改造.
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+
 namespace HelloWorld
 {
     class Hello
@@ -16,14 +16,14 @@ namespace HelloWorld
             Console.WriteLine("モンスターと戦闘中・・・  ");
 
             hp = hp - 5;
-            Console.WriteLine("負傷したぞ！ 5のダメージ！ HPが5減った ");
+            Console.WriteLine("負傷した、 5のダメージ HPが5減った  ");
             Console.WriteLine("HP = " + hp);
 
-            Console.WriteLine("魔法攻撃をした！ 残りのMPは○○。 ");
-            mp = mp - 5;
+            Console.WriteLine("魔法で攻撃する、MPが5減る  ");
+            mp = mp - 3;
             Console.WriteLine("MP = " + mp);
 
-            Console.WriteLine("モンスターを倒した、 経験値を2ポイント獲得した ");
+            Console.WriteLine("モンスターを倒した、 経験値を2ポイント獲得した");
             exp_ = exp_ + 2;
 
             return 0;
@@ -31,7 +31,7 @@ namespace HelloWorld
 
         static int heal()
         {
-            Console.WriteLine("傷薬で回復  \n");
+            Console.WriteLine("傷薬で回復だ  \n");
             hp = hp + 3;
             Console.WriteLine("HP = " + hp);
 
@@ -43,96 +43,54 @@ namespace HelloWorld
         }
 
         static void Main()
-        { 
-
-            hp = 30; mp = 53; exp_ = 0;
+        {
+            hp = 30; mp = 25; exp_ = 0;
             kizugusuri_kosuu = 10;
 
             hp = 30; mp = 25; exp_ = 0;
             kizugusuri_kosuu = 10;
 
-            Console.WriteLine("冒険を始めた。 最初のHPは" + hp + "、MPは" + mp + "。経験値は" + exp_);
+            Console.WriteLine("冒険を始めた、 最初のHPは" + hp + "、MPは" + mp + "。経験値は" + exp_);
 
-            Console.WriteLine("モンスターに遭遇した、 戦闘  ");
+            Console.WriteLine("モンスターに遭遇した、 戦闘 ");
             battle();
-            Console.WriteLine("戦闘終了後のHPは" + hp + "、残りのMPは" + mp + "。経験値は" + exp_);
+            Console.WriteLine("戦闘終了後のHPは" + hp + "、残りMPは" + mp + "。経験値は" + exp_);
 
             Console.WriteLine("回復アイテムを使用する  ");
             heal();
-            Console.WriteLine("回復後のHPは" + hp + "。MPは" + mp + "。経験値は" + exp_);
+            Console.WriteLine("回復後のHPは" + hp + "。残りMPは" + mp + "。経験値は" + exp_);
 
             Console.ReadKey();
         }
     }
-}
-public class Boss
-{
-    // Use this for initialization
-    void Start()
+    public class Boss : BossBase
     {
-        // 要素数5の配列を初期化する
-        int[] points = new int[53];
 
-        // 配列の各要素に値を代入する
-        points[0] = 3;
-        points[1] = 8;
-        points[2] = 6;
-        points[3] = 1;
-        points[4] = 5;
-
-        // 配列の要素をすべて表示する
-        for (int i = 0; i < 5; i++)
+        public class Lesson4 : MonoBehaviour
         {
-            Debug.Log(points[i]);
-        }
-    }
+            // Use this for initialization
+            void Start()
+            {
+                // 要素数5の配列を初期化する
+                int[] arriy = new int[5];
 
-    private int hp = 30;          // 体力
-    private int power = 25; // 攻撃力
+                // 配列の各要素に値を代入する
+                arriy[0] = 3;
+                arriy[1] = 8;
+                arriy[2] = 6;
+                arriy[3] = 1;
+                arriy[4] = 5;
 
-
-    // mpを53で初期化する
-    int Mp = 53;
-    private Action<int> magic;
-
-    // 攻撃用の関数
-    public void Attack()
-    {
-        Debug.Log(this.power + "のダメージを与えた");
-    }
-
-    // 防御用の関数
-    public void Defence(int damage)
-    {
-        Debug.Log(damage + "のダメージを受けた");
-        // 残りhpを減らす
-        this.hp -= damage;
-    }
-
-    //魔力用の関数
-    public void MP(int magic)
-    {
-        // 残りmpを減らす
-        this.magic -= MP;
-    }
-
-
-
-
-public class Test : MonoBehaviour
-    {
-
-        void Start()
-        {
-            // Bossクラスの変数を宣言してインスタンスを代入
-            Boss lastboss = new Boss();
-
-            // 攻撃用の関数を呼び出す
-            lastboss.Attack();
-            // 防御用の関数を呼び出す
-            lastboss.Defence(5);
-            // 魔法用の関数を呼び出すs
-            lastboss.magic(53);
+                // 配列の要素をすべて表示する
+                for (int i = 0; i < 5; i++)
+                {
+                    Debug.Log(arriy[i]);
+                }
+                for (int b = 4; b < 5; b--)
+                {
+                    Debug.Log(arriy[b]);
+                }
+            }
 
             // 消費前とと消費後の値を引いた値を返す関数
             int Add(int mp, int magic)
@@ -141,11 +99,87 @@ public class Test : MonoBehaviour
                 int Mp = mp - magic;
                 // 変数cを呼び出し元の関数に返す
                 return Mp;
+            }
 
-                // Add関数に「5」と「55」の引数を渡し、返り値をnum変数に代入する
-                int num = Add(5, 55);
-                // numを表示する
-                Debug.Log(num);
+            private const int V = 53;
+            private const string V1 = "魔法攻撃をした";
+            private const int V2 = 100;
+            private int power = 25; // 攻撃力
+
+            public Lesson4(int power)
+            {
+                this.power = power;
+            }
+
+            // mpを53で初期化する
+            int mp = V;
+            // 攻撃用の関数
+            public void Attack()
+            {
+                Debug.Log(this.power + "のダメージを与えた");
+            }
+
+
+            // 防御用の関数
+            public void Defence(int damage)
+            {
+                Debug.Log(damage + "のダメージを受けた");
+                // 残りhpを減らす
+                this.mp -= damage;
+            }
+            // 魔力用の関数
+            internal object magic(int v)
+            {
+
+                // 残りmpを減らす
+                int v1 = this.mp - mp;
+                throw new NotImplementedException();
+            }
+        }
+
+        internal void Defence(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void magic(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Lesson4 : MonoBehaviour
+    {
+        private const int V = 53;
+        private const int V1 = 1;
+
+        void Start()
+        {
+            // 10回繰り返す
+            for (int magic = 5 - V1; magic
+                                     >= 5; magic--)
+            {
+                Debug.Log(message: magic + "魔法攻撃をした。残りのmpは○○。");
+            }
+
+            // Bossクラスの変数を宣言してインスタンスを代入
+            Boss lastboss = new Boss();
+
+            // 攻撃用の関数を呼び出す
+            lastboss.Attack();
+            // 防御用の関数を呼び出す
+            lastboss.Defence(3);
+            // 魔力用の関数を呼び出す
+            lastboss.magic(V);
+
+            // 1回処理を繰り返す
+            for (int mp = 5 - 1; mp >= 5; mp--)
+            {
+                Debug.Log(mp + "MPが足りないため、魔法が使えない。");
             }
         }
     }
